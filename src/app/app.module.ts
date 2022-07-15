24418453243f6bf1f3657c069e8ca288f4c5265a
Judt Eric Alan <Eric.Judt@supervielle.com.ar>
2022-07-15 20:31:36 -0300

    feature/component ultimo push punto 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-/*import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here*/
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent /*,
-    HeroDetailComponent*/
+    HeroDetailComponent 
   ],
   imports: [
     BrowserModule,
